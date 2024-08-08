126254938827d8652732b704a938cb7df6f35226
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-08-08 18:48:50 +0900

    Initial Commit

---
+++ DoNotTouchTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoNotTouchTestApp
+//
+//  Created by 伊佐治恵介 on 2024/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoNotTouchTestApp/DoNotTouchTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoNotTouchTestAppApp.swift
+//  DoNotTouchTestApp
+//
+//  Created by 伊佐治恵介 on 2024/08/08.
+//
+
+import SwiftUI
+
+@main
+struct DoNotTouchTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
