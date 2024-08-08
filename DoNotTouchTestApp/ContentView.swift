8fec0a0b2d5a7d1ebebd6543519964f26c826037
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2024-08-08 19:14:49 +0900

    非使用検知検証コードを分離させた

--- DoNotTouchTestApp/ContentView.swift
+++ DoNotTouchTestApp/ContentView.swift
@@ -1,24 +1,51 @@
-//
-//  ContentView.swift
-//  DoNotTouchTestApp
-//
-//  Created by 伊佐治恵介 on 2024/08/08.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var appState: AppState
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("非アクティブ: \(appState.inActiveCount.description)")
+                .foregroundColor(.red)
+                .padding()
         }
-        .padding()
+        .onChange(of: appState.isActive) {
+            if !appState.isActive {
+                showAlert()
+            }
+        }
+    }
+
+
+    func showAlert() {
+        print("App moved to background.")
     }
 }
 
+class AppState: ObservableObject {
+    @Published var isActive: Bool = true
+    @Published var inActiveCount: Int = 0
+
+    init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAppBecameActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleAppResignActive), name: UIApplication.willResignActiveNotification, object: nil)
+    }
+
+    @objc func handleAppBecameActive() {
+        DispatchQueue.main.async {
+            self.isActive = true
+        }
+    }
+
+    @objc func handleAppResignActive() {
+        DispatchQueue.main.async {
+            self.isActive = false
+            self.inActiveCount += 1
+        }
+    }
+}
+
+
 #Preview {
-    ContentView()
+    ContentView().environmentObject(AppState())
 }
